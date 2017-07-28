e09516666ef6383ff3ee5667935e4f09bfbc9e59
Peter Etelej <peter@etelej.com>
2017-07-28 07:43:38 +0300

    Write better dotfile import function

--- resetfiles.sh
+++ resetfiles.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 echo "Installing dotfiles into current profile"
-cp ./.[^.]* ~
+files=($(ls -af|awk '/^\./ && ! /\.$/'))
+for f in $files
+do
+	cp ./${f} ~/.
+
+	if [ $? -ne 0 ]
+	then
+		echo "ERROR: failed to copy ${f} to ~/."
+		exit 2
+	fi
+done
 
 echo "dotfiles reset successfully"
 cd
